932944416aef68df9efad88d87f663ffa9b36f38
Michael Ansbro <mransbro@gmail.com>
2020-05-13 16:11:42 +0100

    First working version

--- stockinfo.py
+++ stockinfo.py
@@ -1,12 +1,49 @@
 import yfinance as yf
 
 import xlsxwriter
-from tkinter import *
 
-top = Tk()
-L1 = Label(top, text = "Stock symbol")
-L1.pack( side = LEFT)
-E1 = Entry(top, bd = 5)
-E1.pack(side = RIGHT)
+import PySimpleGUI as sg
 
-top.mainloop()
+"""
+Program to output data about a stock
+"""
+
+
+def main():
+    layout = [
+        [sg.Text('Stock Research')],
+        [sg.Text('Enter stock symbol'), sg.InputText()],
+        [sg.Text('Choose A Folder', size=(35, 1))],
+        [sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),
+        sg.InputText('Default Folder'), sg.FolderBrowse()],
+        [sg.Button('Ok'), sg.Button('Cancel')]
+    ]
+
+    window = sg.Window('Stock Research', layout)
+
+    while True:
+        event, values = window.read()
+        if event in (None, 'Cancel'):
+            break
+        elif event == 'Ok':
+            i = stockInfo(values[0])
+            saveInfo(i, values[1])
+    window.close()
+
+
+def stockInfo(symbol):
+
+    s = yf.Ticker(symbol)
+    name = s.info['shortName']
+    return(name)
+
+
+def saveInfo(info, path):
+    filepath = path + '/' + info
+    with open(filepath, 'w') as f:
+        f.write(info)
+
+    sg.popup(f'Stock info for {info} saved to {filepath} ')
+
+
+main()
